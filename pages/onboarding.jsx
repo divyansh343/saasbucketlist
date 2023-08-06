417ed1fe082f04ec7b84f8d81c41a1251ca1e4d6
divyansh343 <divyanshpal402@gmail.com>
2023-08-06 20:53:50 +0530

    not fixed still

--- pages/onboarding.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-import Onboarding from '../components/onboaeding/Onboarding'
-const onboarding = () => {
-  return (
-   <Onboarding />
-  )
-}
-
-export default onboarding
